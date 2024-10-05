b6c279dc8b9429435ab21761e268138fe846ea74
Berke Arda <nberkearda@gmail.com>
2024-10-05 11:31:20 +0200

    Initial commit with project files

---
+++ open_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Navigate to your project directory
+cd /Users/berkearda/Desktop/cv1/lab02-local-features
+
+# Activate virtual environment
+source venv/bin/activate
+
+#  Print a message to confirm you're ready to work
+echo "Project environment activated and ready!"
+
